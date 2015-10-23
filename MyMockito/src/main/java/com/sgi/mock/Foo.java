a05aef4b4ea464a9a3391c21d084fb5e5d870116
dharshanshastry <nagarad@blr-nagarad-m.blr.ds.adp.com>
2015-10-23 16:13:51 +0530

    Initial Check-in

---
+++ MyMockito/src/main/java/com/sgi/mock/Foo.java
@@ -0,0 +1,18 @@
+package com.sgi.mock;
+
+
+/**
+ * Just a demo class.
+ *
+ */
+public class Foo {
+
+    public String foo() {
+        return "foo";
+    }
+
+    public String echo(String val) {
+        return val;
+    }
+
+}
---
+++ MyMockito/src/main/java/com/sgi/mock/FooInterface.java
@@ -0,0 +1,13 @@
+package com.sgi.mock;
+
+/**
+ * Just a demo interface.
+ *
+ */
+public interface FooInterface {
+
+    public String foo();
+
+    public String echo(String val);
+
+}
